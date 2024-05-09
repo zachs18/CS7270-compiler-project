522e0640754567c57d2a82e1d73419f6d33e2c43
Zachary S <zasample18+github@gmail.com>
2024-05-08 19:01:27 -0500

    Add GDB running capability

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -29,33 +29,36 @@ fn compile_file(
 }
 
 fn main() {
-    let mut args: Vec<String> = std::env::args().skip(1).collect();
-
-    let program_args = match args.iter().position(|arg| arg == "--") {
-        Some(position) => {
-            let program_args = args.split_off(position + 1);
-            args.pop();
-            program_args
-        }
-        None => vec![],
-    };
-
-    if args.iter().any(|arg| arg.starts_with("--")) {
-        usage()
-    }
+    let args: Vec<String> = std::env::args().skip(1).collect();
 
     let mut source_files = vec![];
     let mut c_source_files = vec![];
+    let mut seen_dash_dash = false;
+    let mut program_args = vec![];
+    let mut gdb = false;
 
-    for file in args {
-        let file = PathBuf::from(file);
-        match file.extension().and_then(OsStr::to_str) {
-            Some("src") => source_files.push(file),
-            Some("c") => c_source_files.push(file),
-            _ => panic!(
-                "source files should end in .src, C helper files should end \
-                 in .c"
-            ),
+    for arg in args {
+        if arg.starts_with("--") {
+            if arg == "--" {
+                seen_dash_dash = true;
+            } else if arg == "--gdb" {
+                gdb = true;
+            } else {
+                eprintln!("unrecognized option: {arg:?}");
+                usage();
+            }
+        } else if !seen_dash_dash {
+            let file = PathBuf::from(arg);
+            match file.extension().and_then(OsStr::to_str) {
+                Some("src") => source_files.push(file),
+                Some("c") => c_source_files.push(file),
+                _ => panic!(
+                    "source files should end in .src, C helper files should \
+                     end in .c"
+                ),
+            }
+        } else {
+            program_args.push(arg);
         }
     }
 
@@ -75,19 +78,31 @@ fn main() {
     let gcc_command_status = Command::new("riscv64-linux-gnu-gcc")
         .arg("-o")
         .arg(executable_file.as_path())
+        .arg("-g")
         .args(compiler_files)
         .status()
         .expect("failed to compile executable");
 
     assert!(gcc_command_status.success(), "failed to compile executable");
 
-    let qemu_command_status = Command::new("qemu-riscv64")
+    let mut qemu_command = Command::new("qemu-riscv64");
+    if gdb {
+        qemu_command.args(["-g", "1234"]);
+        println!(
+            "Start gdb with `gdb-multiarch {}`",
+            executable_file.to_string_lossy()
+        );
+        println!("then run `target remote :1234`");
+    }
+
+    qemu_command
         .args(["-L", "/usr/riscv64-linux-gnu/"])
         .arg(executable_file.as_path())
         .arg("--")
-        .args(program_args)
-        .status()
-        .expect("failed to run executable");
+        .args(program_args);
+
+    let qemu_command_status =
+        qemu_command.status().expect("failed to run executable");
 
     println!("qemu returned {:?}", qemu_command_status.code());
 }
